10e401e71b51ac7c2b3afa5008228e861ed611d1
Erick <erick777wild@comunidad.unam.mx>
2019-11-09 11:06:37 -0600

    funcion de transferencia

---
+++ my_tf2ss.m
@@ -0,0 +1,30 @@
+%% Programa que obtiene la función de transferencia
+
+r = 1;
+
+J1 = 1;
+J2 = 1;
+D1 = 1;
+
+Ft = 1;
+
+N1 = 1;
+N2 = 1;
+N12 = N1/N2;
+
+N21 = N2/N1;
+
+N21_sqr = N21^2;
+
+%Organizo todas las operaciones para ingresarlas a la tf
+coef_r = r;
+coef_S2 = N12*(J1+J2(N21_sqr));
+coef_S = D1+Ft(N21_sqr);
+
+%Aquí debo de normalizar por eso divido todo entre coef_S2
+num = [coef_r/coef_S2]; %Defino el numerado 
+den = [coef_S2/coef_S2 coef_S/coef_S2]; %Defino el denominador
+G = tf(num,den) %Defino mi función de transferencia
+%[A,B,C,D] = ssdata(G); %Defino mi espacio de estados
+sys = ss(G);
+step(sys);
